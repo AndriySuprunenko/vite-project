d5b92408ff9e6989d82d9ad215647bced144b4c9
AndriySuprunenko <43989070+AndriySuprunenko@users.noreply.github.com>
2023-12-19 18:46:05 +0200

    add dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,32 @@
+// Imports
+import { useState } from 'react';
+
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world</h1>
-    </>
+    <div className={`${darkMode && 'dark'}`}>
+      <main className='bg-white dark:bg-black h-screen'>
+        <h1 className='text-3xl font-bold w-fit m-auto dark:text-white'>
+          Hello world
+        </h1>
+        <p className='max-w-sm m-auto mt-5 dark:text-white'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo tempora
+          ab doloribus non assumenda recusandae sit officiis rem maxime. Cum
+          debitis quidem expedita harum culpa corrupti pariatur ad veritatis
+          quibusdam?
+        </p>
+        {/* Button for dark mode */}
+        <button
+          className='absolute text-5xl w-16 h-12 bottom-16 right-16 bg-neutral-900 dark:bg-neutral-50 rounded-full text-white dark:text-black'
+          onClick={toggleDarkMode}
+        >
+          {darkMode ? '🌞' : '🌚'}
+        </button>
+      </main>
+    </div>
   );
 }
 
